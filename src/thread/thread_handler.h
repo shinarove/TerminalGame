2c012aa018a3947e0d2852a5812535bb4c915013
freinin1 <freinin1@students.zhaw.ch>
2025-05-01 11:53:24 +0200

    add documentation in thread_handler.h

--- src/thread/thread_handler.h
+++ src/thread/thread_handler.h
@@ -1,6 +1,12 @@
 #ifndef THREAD_HANDLER_H
 #define THREAD_HANDLER_H
 
+/**
+ * Starts a new thread with the given function.
+ * The thread will be detached, so it will run independently.
+ *
+ * @param thread_func A simple function pointer to the function that will be executed in the thread.
+ */
 void start_simple_thread(void (*thread_func)(void));
 
 #endif//THREAD_HANDLER_H
