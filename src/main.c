dd35b8860412fe760206e3aff7c07195b16db9ac
freinin1 <freinin1@students.zhaw.ch>
2025-05-01 21:58:42 +0200

    add seed generation in main.c

--- src/main.c
+++ src/main.c
@@ -2,6 +2,9 @@
 #include "logger/logger.h"
 #include "../termbox2/termbox2.h"
 
+#include <time.h>
+#include <stdlib.h>
+
 void init();
 void shutdown();
 
@@ -9,6 +12,13 @@ void init() {
     tb_init();
     init_logger();
     init_input_handler();
+
+    // Seed the random number generator with a combination of time, process ID, and stack variable address
+    unsigned int seed = (unsigned int) time(NULL); // Use current time as seed
+    seed ^= (unsigned int) getpid(); // XOR with process ID
+    int stack_var;
+    seed ^= (unsigned int) &stack_var; // XOR with the address of a stack variable
+    srand(seed); // Seed the random number generator
 }
 
 int main(void) {
