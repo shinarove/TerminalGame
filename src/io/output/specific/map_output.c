dcd8a15e9545d856821ff8eeb70c5cc1156583c7
freinin1 <freinin1@students.zhaw.ch>
2025-05-02 13:32:54 +0200

    make map_output not dependent on map.h

--- src/io/output/specific/map_output.c
+++ src/io/output/specific/map_output.c
@@ -4,21 +4,21 @@
 #include "../../../logger/logger.h"
 #include "../../colors.h"
 
-void print_map(const vector2d_t anchor, const parsed_map_t* map) {
+void print_map(const int x, const int y, const parsed_map_t* map) {
     RETURN_WHEN_NULL(map, , "Map Output", "Map is NULL");
 
-    int anchor_x = anchor.dx;
-    int anchor_y = anchor.dy;
-    if (anchor.dx < 0 || anchor.dy < 0) {
-        log_msg(ERROR, "Map Output", "Invalid anchor position: (%d, %d), set to (0, 0)", anchor.dx, anchor.dy);
+    int anchor_x = x;
+    int anchor_y = y;
+    if (x < 0 || y < 0) {
+        log_msg(ERROR, "Map Output", "Invalid anchor position: (%d, %d), set to (0, 0)", x, y);
         anchor_x = 0;
         anchor_y = 0;
     }
 
-    for (int x = 0; x < map->width; x++) {
-        for (int y = 0; y < map->height; y++) {
-            const parsed_map_tile_t tile = map->tiles[x * map->height + y];
-            tb_printf(anchor_x + x, anchor_y + y, tile.foreground_color, tile.background_color, "%c", tile.symbol);
+    for (int i = 0; i < map->width; i++) {
+        for (int j = 0; j < map->height; j++) {
+            const parsed_map_tile_t tile = map->tiles[i * map->height + j];
+            tb_printf(anchor_x + i, anchor_y + j, tile.foreground_color, tile.background_color, "%c", tile.symbol);
         }
     }
 }
--- src/io/output/specific/map_output.h
+++ src/io/output/specific/map_output.h
@@ -1,15 +1,15 @@
 #ifndef MAP_OUTPUT_H
 #define MAP_OUTPUT_H
 
-#include "../../../game_data/map/map.h"
 #include "../../../game_data/map/map_parser.h"
 
 /**
  * Prints the provided map on the console, starting from the specified anchor point.
  *
- * @param anchor The starting position (x, y) on the console where the map will begin drawing.
+ * @param x The x-coordinate of the anchor point where the map will be printed.
+ * @param y The y-coordinate of the anchor point where the map will be printed.
  * @param map A pointer to the parsed map tile structure containing the tile symbols and their associated colors.
  */
-void print_map(vector2d_t anchor, const parsed_map_t* map);
+void print_map(int x, int y, const parsed_map_t* map);
 
 #endif//MAP_OUTPUT_H
