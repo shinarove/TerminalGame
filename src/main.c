aeaed028746fca9b49463d8fa28201f39dc885e5
freinin1 <freinin1@students.zhaw.ch>
2025-05-02 14:55:53 +0200

    init_memory_pool in main

--- src/main.c
+++ src/main.c
@@ -1,6 +1,8 @@
 #include "../termbox2/termbox2.h"
 #include "io/input/input_handler.h"
 #include "logger/logger.h"
+#include "memory/mem_mgmt.h"
+#include "game.h"
 
 #include <stdlib.h>
 #include <time.h>
@@ -23,9 +25,13 @@ void init() {
 
 int main(void) {
     init();
+    memory_pool_t* pool = init_memory_pool(STANDARD_MEMORY_POOL_SIZE);
+    //the memory pool doesn't need to be checked here, as it will be checked in start_game_loop()
 
     log_msg(FINE, "Main", "This is a test log message");
+    start_game_loop(pool);
 
+    shutdown_memory_pool(pool);
     shutdown();
     return 0;
 }
