e017f172cd4125636c7699b8e05c7fd3266ab31b
freinin1 <freinin1@students.zhaw.ch>
2025-05-02 13:36:57 +0200

    implement simple print function

--- src/io/output/common/common_output.c
+++ src/io/output/common/common_output.c
@@ -1,1 +1,10 @@
 #include "common_output.h"
+
+#include "../../../logger/logger.h"
+#include "../../../../termbox2/termbox2.h"
+
+void print_text(const int x, const int y, const color_t fg, const color_t bg, const char* text) {
+    RETURN_WHEN_NULL(text, , "Common Output", "print_text: Text is NULL");
+
+    tb_printf(x, y, color_mapping[fg].value, color_mapping[bg].value, "%s", text);
+}
--- src/io/output/common/common_output.h
+++ src/io/output/common/common_output.h
@@ -1,4 +1,8 @@
 #ifndef COMMON_OUTPUT_H
 #define COMMON_OUTPUT_H
 
+#include "../../colors.h"
+
+void print_text(int x, int y, color_t fg, color_t bg, const char* text);
+
 #endif//COMMON_OUTPUT_H
