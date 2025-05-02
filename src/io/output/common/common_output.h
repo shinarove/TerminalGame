f0ea60c49719d1fbeafbdc9e2ab959e10586e270
freinin1 <freinin1@students.zhaw.ch>
2025-05-02 13:59:40 +0200

    add doc to common_output.h

--- src/io/output/common/common_output.h
+++ src/io/output/common/common_output.h
@@ -3,6 +3,18 @@
 
 #include "../../colors.h"
 
+/**
+ * Prints a text string at the specified position on the terminal with the specified foreground and background colors.
+ *
+ * @param x The x-coordinate where the text will be placed.
+ * @param y The y-coordinate where the text will be placed.
+ * @param fg The foreground color of the text, specified as a value of type color_t.
+ * @param bg The background color of the text, specified as a value of type color_t.
+ * @param text A pointer to a null-terminated string containing the text to be displayed.
+ *
+ * The function logs an error and returns if the provided `text` is NULL. It uses the termbox library to output the
+ * colored text by mapping the color_t enumeration values to corresponding termbox color attributes.
+ */
 void print_text(int x, int y, color_t fg, color_t bg, const char* text);
 
 #endif//COMMON_OUTPUT_H
