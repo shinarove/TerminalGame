5ac081ddf7596dc6c268b8db3cb21e803bff76fb
freinin1 <freinin1@students.zhaw.ch>
2025-05-05 08:50:03 +0200

    update handle map event function

--- src/game_modes/map/map_event_handler.c
+++ src/game_modes/map/map_event_handler.c
@@ -2,41 +2,28 @@
 
 #include "../../logger/logger.h"
 
-/**
- * Handles fountain events on the map, replenishing the player's health, mana, or stamina
- * based on the type of fountain they interact with. The fountain tile is then
- * replaced with a FLOOR tile to indicate it has been used.
- *
- * @param map A pointer to the map structure containing the player's position and tiles information.
- * @param player A pointer to the character structure representing the player whose resources may be affected.
- */
-void handle_fountain_event(const map_t* map, character_t* player);
+void handle_fountain_event(const map_t* map, void (*reset_func)(character_t*), character_t* player);
 
 state_t handle_map_event(const map_t* map, character_t* player) {
-    RETURN_WHEN_NULL(map, , "Map Event Handler", "Map is NULL");
-    RETURN_WHEN_NULL(player, , "Map Event Handler", "Player is NULL");
+    RETURN_WHEN_NULL(map, MAP_MODE, "Map Event Handler", "Map is NULL")
+    RETURN_WHEN_NULL(player, MAP_MODE, "Map Event Handler", "Player is NULL")
 
-    handle_fountain_event(map, player);
-    return MAP_MODE;
-}
-
-void handle_fountain_event(const map_t* map, character_t* player) {
     const int player_on_map_idx = map->player_pos.dx * map->height + map->player_pos.dy;
     const map_tile_t tile = map->hidden_tiles[player_on_map_idx];
 
+    state_t next_state = MAP_MODE;
     switch (tile) {
         case LIFE_FOUNTAIN:
-            reset_health_c(player);
-            map->hidden_tiles[player_on_map_idx] = FLOOR;
-            map->revealed_tiles[player_on_map_idx] = FLOOR;
-            break;
-        case MANA_FOUNTAIN:
-            reset_mana_c(player);
-            map->hidden_tiles[player_on_map_idx] = FLOOR;
-            map->revealed_tiles[player_on_map_idx] = FLOOR;
+            handle_fountain_event(map, reset_health_c, player);
             break;
         case STAMINA_FOUNTAIN:
-            reset_stamina_c(player);
+            handle_fountain_event(map, reset_stamina_c, player);
+            break;
+        case MANA_FOUNTAIN:
+            handle_fountain_event(map, reset_mana_c, player);
+            break;
+        case ENEMY:
+            next_state = COMBAT_MODE;
             map->hidden_tiles[player_on_map_idx] = FLOOR;
             map->revealed_tiles[player_on_map_idx] = FLOOR;
             break;
@@ -44,4 +31,13 @@ void handle_fountain_event(const map_t* map, character_t* player) {
             // do nothing
             break;
     }
+    return next_state;
+}
+
+void handle_fountain_event(const map_t* map, void (*reset_func)(character_t*), character_t* player) {
+    const int player_on_map_idx = map->player_pos.dx * map->height + map->player_pos.dy;
+
+    reset_func(player);
+    map->hidden_tiles[player_on_map_idx] = FLOOR;
+    map->revealed_tiles[player_on_map_idx] = FLOOR;
 }
