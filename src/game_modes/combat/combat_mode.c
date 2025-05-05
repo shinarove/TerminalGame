8fedd46f2ff7fae746ae1f36664eae3abd5f56d5
freinin1 <freinin1@students.zhaw.ch>
2025-05-05 15:16:30 +0200

    add functions in combat_mode

--- src/game_modes/combat/combat_mode.c
+++ src/game_modes/combat/combat_mode.c
@@ -1,1 +1,46 @@
 #include "combat_mode.h"
+
+#include "../../logger/logger.h"
+#include "../../io/local/local_handler.h"
+
+#include <stdlib.h>
+
+enum combat_mode_index {
+    MESSAGE1,
+    MAX_COMBAT_MODE_INDEX
+};
+
+char** combat_mode_strings = NULL;
+
+void update_combat_mode_local(void);
+
+int init_combat_mode() {
+    combat_mode_strings = (char**) malloc(sizeof(char*) * MAX_COMBAT_MODE_INDEX);
+    RETURN_WHEN_NULL(combat_mode_strings, 1, "Combat Mode", "Failed to allocate memory for combat mode strings.");
+
+    for (int i = 0; i < MAX_COMBAT_MODE_INDEX; i++) {
+        combat_mode_strings[i] = NULL;
+    }
+
+    update_combat_mode_local();
+    observe_local(update_combat_mode_local);
+    return 0;
+}
+
+state_t update_combat_mode(input_t input, character_t* player) {
+
+    return COMBAT_MODE;
+}
+
+void shutdown_combat_mode() {
+    if (combat_mode_strings != NULL) {
+        for (int i = 0; i < MAX_COMBAT_MODE_INDEX; i++) {
+            free(combat_mode_strings[i]);
+        }
+        free(combat_mode_strings);
+    }
+}
+void update_combat_mode_local(void) {
+
+}
+
--- src/game_modes/combat/combat_mode.h
+++ src/game_modes/combat/combat_mode.h
@@ -1,4 +1,13 @@
 #ifndef COMBAT_MODE_H
 #define COMBAT_MODE_H
 
+#include "../../game.h"
+#include "../../io/input/input_handler.h"
+
+int init_combat_mode(void);
+
+state_t update_combat_mode(input_t input, character_t* player);
+
+void shutdown_combat_mode(void);
+
 #endif//COMBAT_MODE_H
