07ef78076c83ab8d01e28edf8496204a0b41cd15
freinin1 <freinin1@students.zhaw.ch>
2025-05-07 10:41:41 +0200

    Add `parse_float` function to string_helper

--- src/helper/string_helper.c
+++ src/helper/string_helper.c
@@ -8,8 +8,18 @@ int parse_int(const char* str, int* out) {
 
     if (*end != '\0' || end == str) {
         return -1;// Error: not a valid integer
-    } else {
-        *out = (int) val;
-        return 0;// Success
     }
+    *out = (int) val;
+    return 0;// Success
+}
+
+int parse_float(const char* str, float* out) {
+    char* end;
+    const double val = strtod(str, &end);
+
+    if (*end != '\0' || end == str) {
+        return -1;// Error: not a valid float
+    }
+    *out = (float) val;
+    return 0;// Success
 }
--- src/helper/string_helper.h
+++ src/helper/string_helper.h
@@ -4,4 +4,6 @@
 
 int parse_int(const char* str, int* out);
 
+int parse_float(const char* str, float* out);
+
 #endif//STRING_HELPER_H
