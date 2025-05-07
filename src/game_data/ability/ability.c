f1a041cedaf5abed8a38b9eb94adcd96265c3a92
freinin1 <freinin1@students.zhaw.ch>
2025-05-07 10:41:59 +0200

    Expand ability system to include detailed attributes

--- src/game_data/ability/ability.c
+++ src/game_data/ability/ability.c
@@ -8,7 +8,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-
 #ifdef _WIN32
     #define PATH_SEP "\\"
     #define ABILITY_DIRECTORY "ressources\\game_data\\ability"
@@ -32,10 +31,9 @@ ability_table_t* init_ability_table(const memory_pool_t* pool) {
         singleton_ability_table->count = MAX_ABILITIES;
 
         char rel_path[64];
-        snprintf(rel_path, sizeof(rel_path), "%s" PATH_SEP "%s", ABILITY_DIRECTORY, ABILITY_FILE_NAME);
+        snprintf(rel_path, sizeof(rel_path),
+                 "%s" PATH_SEP "%s", ABILITY_DIRECTORY, ABILITY_FILE_NAME);
         FILE* ability_file = fopen(rel_path, "r");
-
-        int* id_ref = NULL;
 
         char line[256];
         int count = -1;
@@ -44,20 +42,84 @@ ability_table_t* init_ability_table(const memory_pool_t* pool) {
                 count++;
                 continue;// skip the header line
             }
-            id_ref = &singleton_ability_table->abilities[count].id;
             // read the id, convert it to int, and assign it to the ability
             const char* token = strtok(line, ",");
-            RETURN_WHEN_NULL(token, NULL, "Ability", "Failed to read ability id at line %d", count + 1);
-            RETURN_WHEN_TRUE(parse_int(token, id_ref) != 0, NULL, "Ability", "Failed to read ability id at line %d", count + 1);
+            RETURN_WHEN_NULL(token, NULL, "Ability", "Failed to read ability id at line %d", count + 1)
+            RETURN_WHEN_TRUE(parse_int(token, &singleton_ability_table->abilities[count].id) != 0, NULL,
+                             "Ability", "Failed to read ability id at line %d", count + 1)
             // check if the id is valid
-            RETURN_WHEN_TRUE(*id_ref != count, NULL, "Ability", "Invalid ability id %d at line %d, should be %d.", *id_ref, count + 1, count);
+            RETURN_WHEN_TRUE(singleton_ability_table->abilities[count].id != count,
+                             NULL, "Ability", "Invalid ability id %d at line %d, should be %d.",
+                             singleton_ability_table->abilities[count].id, count + 1, count)
 
             // read the name
             token = strtok(NULL, ",");
-            RETURN_WHEN_NULL(token, NULL, "Ability", "Failed to read ability name at line %d", count + 1);
+            RETURN_WHEN_NULL(token, NULL, "Ability", "Failed to read ability name at line %d", count + 1)
             singleton_ability_table->abilities[count].key_name = strdup(token);
-
             singleton_ability_table->abilities[count].local_name = get_local_string(token);
+
+            // read target character
+            token = strtok(NULL, ",");
+            RETURN_WHEN_NULL(token, NULL, "Ability", "Failed to read ability single character at line %d", count + 1)
+            singleton_ability_table->abilities[count].c_target = token[0];
+            // read target resource
+            token = strtok(NULL, ",");
+            RETURN_WHEN_NULL(token, NULL, "Ability", "Failed to read ability target resource at line %d", count + 1)
+            singleton_ability_table->abilities[count].r_target = token[0];
+            // read effect type
+            token = strtok(NULL, ",");
+            RETURN_WHEN_NULL(token, NULL, "Ability", "Failed to read ability effect type at line %d", count + 1)
+            singleton_ability_table->abilities[count].effect_type = token[0];
+            // read effect scaler
+            token = strtok(NULL, ",");
+            RETURN_WHEN_NULL(token, NULL, "Ability", "Failed to read ability effect scaler at line %d", count + 1)
+            singleton_ability_table->abilities[count].effect_scaler = token[0];
+            // read accuracy scaler
+            token = strtok(NULL, ",");
+            RETURN_WHEN_NULL(token, NULL, "Ability", "Failed to read ability accuracy scaler at line %d", count + 1)
+            singleton_ability_table->abilities[count].accuracy_scaler = token[0];
+            // read resource cost
+            token = strtok(NULL, ",");
+            RETURN_WHEN_NULL(token, NULL, "Ability", "Failed to read ability costs resource at line %d", count + 1)
+            singleton_ability_table->abilities[count].r_cost = token[0];
+
+            // read effect dice
+            token = strtok(NULL, ",");
+            RETURN_WHEN_NULL(token, NULL, "Ability", "Failed to read ability effect dice at line %d", count + 1)
+            RETURN_WHEN_TRUE(parse_int(token, &singleton_ability_table->abilities[count].effect_dice) != 0,
+                             NULL, "Ability", "Failed to read ability effect dice at line %d", count + 1)
+            // read accuracy dice
+            token = strtok(NULL, ",");
+            RETURN_WHEN_NULL(token, NULL, "Ability", "Failed to read ability accuracy dice at line %d", count + 1)
+            RETURN_WHEN_TRUE(parse_int(token, &singleton_ability_table->abilities[count].accuracy_dice) != 0,
+                             NULL, "Ability", "Failed to read ability accuracy dice at line %d", count + 1)
+            // read effect rolls
+            token = strtok(NULL, ",");
+            RETURN_WHEN_NULL(token, NULL, "Ability", "Failed to read ability effect rolls at line %d", count + 1)
+            RETURN_WHEN_TRUE(parse_int(token, &singleton_ability_table->abilities[count].effect_rolls) != 0,
+                             NULL, "Ability", "Failed to read ability effect rolls at line %d", count + 1)
+            // read accuracy rolls
+            token = strtok(NULL, ",");
+            RETURN_WHEN_NULL(token, NULL, "Ability", "Failed to read ability accuracy rolls at line %d", count + 1)
+            RETURN_WHEN_TRUE(parse_int(token, &singleton_ability_table->abilities[count].accuracy_rolls) != 0,
+                             NULL, "Ability", "Failed to read ability accuracy rolls at line %d", count + 1)
+
+            // read effect scale value
+            token = strtok(NULL, ",");
+            RETURN_WHEN_NULL(token, NULL, "Ability", "Failed to read ability effect scale value at line %d", count + 1)
+            RETURN_WHEN_TRUE(parse_float(token, &singleton_ability_table->abilities[count].effect_scale_value) != 0,
+                             NULL, "Ability", "Failed to read ability effect scale value at line %d", count + 1)
+            // read accuracy scale value
+            token = strtok(NULL, ",");
+            RETURN_WHEN_NULL(token, NULL, "Ability", "Failed to read ability accuracy scale value at line %d", count + 1)
+            RETURN_WHEN_TRUE(parse_float(token, &singleton_ability_table->abilities[count].accuracy_scale_value) != 0,
+                             NULL, "Ability", "Failed to read ability accuracy scale value at line %d", count + 1)
+
+            // read resource cost
+            token = strtok(NULL, ",");
+            RETURN_WHEN_NULL(token, NULL, "Ability", "Failed to read ability resource cost at line %d", count + 1)
+            RETURN_WHEN_TRUE(parse_int(token, &singleton_ability_table->abilities[count].v_cost) != 0,
+                             NULL, "Ability", "Failed to read ability resource cost at line %d", count + 1)
 
             count++;
         }
--- src/game_data/ability/ability.h
+++ src/game_data/ability/ability.h
@@ -4,10 +4,39 @@
 #include "../../memory/mem_mgmt.h"
 #include "ability_index.h"
 
-typedef struct {
+#define SELF_CHAR 's'
+#define ENEMY_CHAR 'e'
+#define HEALTH_CHAR 'h'
+#define STAMINA_CHAR 's'
+#define MANA_CHAR 'm'
+#define ADDITIV_CHAR 'a'
+#define SUBTRACTIVE_CHAR 's'
+#define STRENGTH_CHAR 's'
+#define INTELLIGENCE_CHAR 'i'
+#define AGILITY_CHAR 'a'
+#define ENDURANCE_CHAR 'e'
+#define LUCK_CHAR 'l'
+
+typedef struct ability {
     int id;
     char* key_name;
     char* local_name;
+    char c_target; // 's' for self, 'e' for enemy
+    char r_target; // 'h' for health, 's' for stamina, 'm' for mana
+    char effect_type; // 'd' for damage, 'h' for heal
+    char effect_scaler; // 's' for strength, 'i' for intelligence, 'a' for agility, 'e' for endurance, 'l' for luck
+    char accuracy_scaler; // 's' for strength, 'i' for intelligence, 'a' for agility, 'e' for endurance, 'l' for luck
+    char r_cost; // 'h' for health, 's' for stamina, 'm' for mana
+
+    int effect_dice; // dice to calculate the base effect
+    int accuracy_dice; // the used dice to determine if the user hits the target
+    int effect_rolls; // the number of rolls for the effect dice
+    int accuracy_rolls; // the number of rolls for the accuracy dice
+
+    float effect_scale_value; // this value will be multiplied with the effect_scaler_attr and added to the effect_base_value
+    float accuracy_scale_value; // this value will be multiplied with the accuracy_scaler_attr and added to the rolled accuracy value
+
+    int v_cost; // the cost of the ability in the used resource (costs_resources)
 } ability_t;
 
 typedef struct {
