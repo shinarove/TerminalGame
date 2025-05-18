e8ee86acb306bedf57f3cf1206862e6c01357203
freinin1 <freinin1@students.zhaw.ch>
2025-05-18 10:28:12 +0200

    Add string formatting macros in string_formats.h

---
+++ src/io/string_formats.h
@@ -0,0 +1,11 @@
+#ifndef STRING_FORMATS_H
+#define STRING_FORMATS_H
+
+#define NAME_LVL_FORMAT_C "%s [%s: %d]"
+#define RESOURCE_FORMAT_C "%s: %d/%d"
+#define ATTRIBUTE_FORMAT_C "%s: %d"
+
+#define RES_LINE_FORMAT_C "%s\t| %s\t| %s"
+#define ATTR_LINE_FORMAT_C "%s\t| %s\t| %s\t| %s\t| %s"
+
+#endif //STRING_FORMATS_H
